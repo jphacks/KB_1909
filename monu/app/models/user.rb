ec5cdc1533451be44abe7ee2653f5f4cf4c01b43
face9363 <4181hiro@gmail.com>
2019-10-19 23:56:44 +0900

    remove image

--- monu/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  has_secure_token
-
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :name, presence: true
-end
